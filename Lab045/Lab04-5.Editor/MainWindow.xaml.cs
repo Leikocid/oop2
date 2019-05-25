17fe097708ea194119e3dbc891082188a3978fc7
Leikocid <am.astakhov@gmail.com>
2019-05-25 14:43:04 +0300

    oop45 - начало

---
+++ Lab045/Lab04-5.Editor/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab04_5.Editor {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void RtbEditor_TextChanged(object sender, TextChangedEventArgs e) {
+
+        }
+
+        private void RtbEditor_SelectionChanged(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void SetRussian(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void SetEnglish(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
