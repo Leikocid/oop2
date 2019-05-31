44296359028b68bc04eb256f8d31c0fc94870f2b
Leikocid <am.astakhov@gmail.com>
2019-06-01 01:37:32 +0300

    лаба номер 8

---
+++ Lab08/Lab08/CreateDatabase.sql
@@ -0,0 +1,65 @@
+﻿
+
+USE MASTER
+GO
+
+IF DB_ID('computers') IS NOT NULL
+BEGIN
+    DROP DATABASE computers
+END
+
+
+CREATE DATABASE computers COLLATE Cyrillic_General_CS_AS;
+GO
+
+USE [computers]
+GO
+
+CREATE TABLE [dbo].[Processor](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Model] [varchar](50) NOT NULL,
+	[Developer] [varchar](50) NOT NULL,
+	[CoresCount] int NOT NULL,
+	CONSTRAINT [PK_Processor] PRIMARY KEY (
+		[Id] ASC
+	)
+)
+GO
+
+CREATE TABLE [dbo].[Computer](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Type] [varchar](50) NOT NULL,
+	[Logo] [image] NULL,
+	[ProcessorId] [int] NOT NULL,
+	[MemorySize] [int] NULL,
+	[VideoCard] [varchar](50) NULL,
+	[DiskSize] [int] NULL,
+	CONSTRAINT [PK_Computer] PRIMARY KEY (
+		[Id] ASC
+	)
+)
+GO
+
+ALTER TABLE [dbo].[Computer] WITH CHECK ADD CONSTRAINT [FK_ComputerProcessor] FOREIGN KEY([ProcessorId])
+REFERENCES [dbo].[Processor] ([Id])
+ON DELETE CASCADE
+GO
+
+CREATE PROCEDURE CreateData
+AS
+BEGIN
+	declare @IdentityOutput table ( ID int );
+	declare @id INT;
+
+	insert Processor (Model, Developer, CoresCount) output inserted.Id into @IdentityOutput values ('Core i7', 'intel', 8);
+	select @id = (select ID from @IdentityOutput); delete from @IdentityOutput;
+	insert Computer (Type, ProcessorId, MemorySize, VideoCard, DiskSize) values ('Notebook', @id, 16, 'Nvidia', 512);
+	insert Computer (Type, ProcessorId, MemorySize, VideoCard, DiskSize) values ('Desktop', @id, 32, 'Nvidia', 512);
+	insert Computer (Type, ProcessorId, MemorySize, VideoCard, DiskSize) values ('Ultrabook', @id, 8, 'Nvidia', 256);
+
+	SELECT 'Создана 1 запись в Процессорах и 3 записи в Компьютерах'
+END
+GO
+
+USE MASTER
+GO
---
+++ Lab08/Lab08/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace Lab08 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        public static string connectionString = ConfigurationManager.ConnectionStrings["ComputersConnection"].ConnectionString;
+
+        private SqlDataAdapter adapterProc;
+        private SqlDataAdapter adapterComp;
+        private DataTable dataProc;
+        private DataTable dataComp;
+       
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            RefreshGrids();
+        }
+
+        private void RefreshGrids() {
+            SqlConnection connection = null;
+            try {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                adapterProc = new SqlDataAdapter(new SqlCommand("SELECT * FROM Processor", connection));  
+                dataProc = new DataTable();
+                adapterProc.Fill(dataProc);
+                dgProcessors.ItemsSource = dataProc.DefaultView;
+
+                adapterComp = new SqlDataAdapter(new SqlCommand("SELECT * FROM Computer", connection));
+                dataComp = new DataTable();
+                adapterComp.Fill(dataComp);
+                dgComputers.ItemsSource = dataComp.DefaultView;
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+                if (connection != null) {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void BtnRun_Click(object sender, RoutedEventArgs e) {
+            String sql = txtSql.Text;
+            if (sql != "") {
+                SqlConnection connection = null;
+                try {
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    SqlTransaction tx = null;
+                    try {
+                        SqlCommand command = new SqlCommand(sql, connection);
+                        tx = connection.BeginTransaction();
+                        command.Transaction = tx;
+
+                        SqlDataReader reader = command.ExecuteReader();
+                        string str = "";
+                        for (int i = 0; i < reader.FieldCount; i++) {
+                            str = str + reader.GetName(i) + '\t';
+                        }
+                        str = str + '\n';
+                        if (reader.HasRows) {
+                            while (reader.Read()) {
+                                for (int i = 0; i < reader.FieldCount; i++) {
+                                    str = str + reader.GetValue(i) + '\t';
+                                }
+                            }
+                        }
+                        if (str.Length > 0) {
+                            MessageBox.Show(str);
+                        }
+                        reader.Close();
+
+                        tx.Commit();
+                        RefreshGrids();
+                    } catch (Exception ex) {
+                        MessageBox.Show(ex.Message);
+                        tx.Rollback();
+                    }
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                } finally {
+                    if (connection != null) {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e) {
+            if (dgProcessors.SelectedItems != null) {
+                for (int i = 0; i < dgProcessors.SelectedItems.Count; i++) {
+                    if (dgProcessors.SelectedItems[i] is DataRowView datarowView) {
+                        DataRow dataRow = (DataRow)datarowView.Row;
+                        dataRow.Delete();
+                    }
+                }
+            }
+            if (dgComputers.SelectedItems != null) {
+                for (int i = 0; i < dgComputers.SelectedItems.Count; i++) {
+                    if (dgComputers.SelectedItems[i] is DataRowView datarowView) {
+                        DataRow dataRow = (DataRow)datarowView.Row;
+                        dataRow.Delete();
+                    }
+                }
+            }
+            UpdateDatabase();
+            RefreshGrids();
+        }
+
+        private void UpdateDatabase() {
+            SqlCommandBuilder comandbuilder = new SqlCommandBuilder(adapterComp);  // автоматич генер-т команды, кот позволяют согласовать изменения, 
+            SqlCommandBuilder comandbuilder2 = new SqlCommandBuilder(adapterProc); // вносимые в объект dataset, со связанной бд
+            adapterComp.Update(dataComp);
+            adapterProc.Update(dataProc);
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e) {
+            UpdateDatabase();
+        }
+
+        private void BtnStored_Click(object sender, RoutedEventArgs e) {
+            SqlConnection connection = null;
+            try {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("CreateData", connection) {
+                    CommandType = System.Data.CommandType.StoredProcedure
+                };
+                var reader = command.ExecuteReader();
+                string str = "";
+                for (int i = 0; i < reader.FieldCount; i++) {
+                    str = str + reader.GetName(i) + '\t';
+                }
+                str = str + '\n';
+                if (reader.HasRows) {
+                    while (reader.Read()) {
+                        for (int i = 0; i < reader.FieldCount; i++) {
+                            str = str + reader.GetValue(i) + '\t';
+                        }
+                    }
+                }
+                if (str.Length > 0) {
+                    MessageBox.Show(str);
+                }
+                RefreshGrids();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+                if (connection != null) {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
